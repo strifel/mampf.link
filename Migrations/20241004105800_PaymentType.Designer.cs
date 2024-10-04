b8ddd7c461972f9dc81d23a581cf1bd31f2c7d6f
strifel <info@strifel.de>
2024-10-04 16:34:21 +0200

    Add payment types

--- Migrations/20241004105800_PaymentType.Designer.cs
+++ Migrations/20241004105800_PaymentType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GroupOrder.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GroupOrder.Migrations
 {
     [DbContext(typeof(GroupContext))]
-    partial class GroupContextModelSnapshot : ModelSnapshot
+    [Migration("20241004105800_PaymentType")]
+    partial class PaymentType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");

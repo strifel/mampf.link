215cce429532f48a4bfad30d994d58839ce023db
strifel <info@strifel.de>
2024-10-07 15:40:59 +0200

    Add menu url

---
+++ Migrations/20241007133421_Add Menu.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GroupOrder.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMenu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MenuURL",
+                table: "Groups",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MenuURL",
+                table: "Groups");
+        }
+    }
+}

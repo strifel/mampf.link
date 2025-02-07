5446dabcecc5923479391ed4f317f76b2ca33ca2
David Mehren <git@herrmehren.de>
2025-02-07 20:09:48 +0100

    Migrate AppLayout.razor to code-behind

---
+++ Components/Layout/AppLayout.razor.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using RouteData = Microsoft.AspNetCore.Components.RouteData;
+
+namespace GroupOrder.Components.Layout;
+
+public partial class AppLayout
+{
+    [CascadingParameter]
+    private RouteData? RouteData { get; set; }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        AdminService.Initialize();
+        if (RouteData?.RouteValues.ContainsKey("GroupSlug") == true)
+        {
+            await GroupService.LoadGroup(
+                (string)RouteData?.RouteValues.GetValueOrDefault("GroupSlug")!
+            );
+            StateHasChanged();
+        }
+
+        await base.OnParametersSetAsync();
+        NavManager.LocationChanged += HandleLocationChanged;
+        AdminService.CodeChanged += HandleCodeChanged;
+    }
+
+    private async void HandleLocationChanged(object? sender, LocationChangedEventArgs? e)
+    {
+        if (RouteData?.RouteValues.ContainsKey("GroupSlug") != true)
+            return; // TODO clear
+        await GroupService.LoadGroup(
+            (string)RouteData?.RouteValues.GetValueOrDefault("GroupSlug")!
+        );
+        StateHasChanged();
+    }
+
+    private async void HandleCodeChanged(object? sender, EventArgs e)
+    {
+        await GroupService.ReloadGroup();
+        StateHasChanged();
+    }
+}

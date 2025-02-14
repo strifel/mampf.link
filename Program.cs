7a2bbc137aa62922a27863ff89a036eaaab46868
David Mehren <git@herrmehren.de>
2025-02-14 21:08:18 +0100

    Set up logging with NLog

--- Program.cs
+++ Program.cs
@@ -1,32 +1,54 @@
 using GroupOrder.Components;
 using GroupOrder.Data;
 using GroupOrder.Services.Common;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-// Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-
-builder.Services.AddDbContextFactory<GroupContext>();
-builder.Services.AddScoped<IGroupService, GroupService>();
-builder.Services.AddScoped<IAdminService, GroupAdminService>();
-builder.Services.AddSingleton<IGroupAutoreloadService, GroupAutoreloadService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    var builder = WebApplication.CreateBuilder(args);
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Add services to the container.
+    builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+    builder.Services.AddDbContextFactory<GroupContext>();
+    builder.Services.AddScoped<IGroupService, GroupService>();
+    builder.Services.AddScoped<IAdminService, GroupAdminService>();
+    builder.Services.AddSingleton<IGroupAutoreloadService, GroupAutoreloadService>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-
-app.Run();
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    LogManager.Shutdown();
+}
